59ff885e4f7d58a5eb52243a57f9bc38d2216dca
M4reg <81312045+M4reg@users.noreply.github.com>
2024-10-09 22:37:58 +0200

    Update script.js
    změna

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.getElementById('rangeForm').addEventListener('submit', function(event) 
     allowedNumbers = [...allNumbers];
 
     // Vyloučená čísla
-    const excludedNumbers = [1,3,4,5,6,7,8,9,10];
+    const excludedNumbers = [];
 
     // Filtrace vyloučených čísel z povolených čísel
     allowedNumbers = allowedNumbers.filter(num => !excludedNumbers.includes(num));
